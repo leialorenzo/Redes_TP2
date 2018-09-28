9f00f5cbf39557f4be401d5dd35cb3a505571133
leilalorenzo <41600741+leialorenzo@users.noreply.github.com>
2018-09-28 19:51:08 -0300

    importo datos, limpio a formato red ordeno funcion topologia

--- Intro.py
+++ Intro.py
@@ -39,23 +39,55 @@ L = nx.Graph()
 L.add_edges_from(lit)
 
 LR = nx.Graph()
-LR.add_edges_from(litreg)
+LR.add_edges_from(litr)
 #%%
-def topologia(redes): #dar vector con redes ordenadas
-    G = redes
-    N = np.empty_like(G)          # número de nodos de la red
-    E = np.empty_like(G)          # número de enlaces de la red
-    G_mean = np.empty_like(G)     # grado medio de la red
-    G_max = np.empty_like(G)      # grado máximo
-    G_min  = np.empty_like(G)     # grado mínimo
-    dens = np.empty_like(G)       # densidad de la red
-    clu = np.empty_like(G)        # coeficiente de clustering local
-    clu_d = np.empty_like(G)      # coeficiente de clustering global/transitividad
-    diam = np.empty_like(G)       # diámetro de la red
-    
-    def directed(A):
+
+def directed(A):
     for i in range(0,len(A)):
         for r in range(i+1,len(A)):
             if A[i][0] == A[r][1] and A[i][1] == A[r][0]:
                 return "SI"
     return "NO"
+
+
+def topologia(redes, edges): #dar vector con redes ordenadas y sus bases de datos
+    
+    R = redes
+    L = edges
+    
+    N = np.empty_like(R)          # número de nodos de la red
+    E = np.empty_like(R)          # número de enlaces de la red
+    R_mean = np.empty_like(R)     # grado medio de la red
+    R_max = np.empty_like(R)      # grado máximo
+    R_min  = np.empty_like(R)     # grado mínimo
+    dens = np.empty_like(R)       # densidad de la red
+    clu = np.empty_like(R)        # coeficiente de clustering local
+    clu_d = np.empty_like(R)      # coeficiente de clustering global/transitividad
+    diam = np.empty_like(R)       # diámetro de la red
+    
+    for i in range(len(R)):
+        N[i] = R[i].number_of_nodes()
+        E[i] = R[i].number_of_edges()
+        R_mean[i] = np.mean([a[1] for a in list(R[i].degree())])
+        R_max[i] = np.max([a[1] for a in list(R[i].degree())])
+        R_min[i] = np.min([a[1] for a in list(R[i].degree())])
+        dens[i] = nx.density(R[i])
+        clu[i] = nx.average_clustering(R[i])
+        clu_d[i] = nx.transitivity(R[i])
+        diam[i] = nx.diameter(max(nx.connected_component_subgraphs(R[i]), key=len))
+    
+    dirigido = np.empty_like(R)
+    for i in range(len(dirigido)):
+        dirigido[i] = directed(L[i])
+        
+    return N, E, R_mean, R_max, R_min, dens, clu, clu_d, diam, dirigido
+#%%
+A = topologia([P,B,L,LR], [prot, bina, lit, litr])
+#%%
+tabla = pd.DataFrame({"Red":["Binarias","Proteicas","Literatura", "Literatura Regulada"],"# de nodos":A[0],"# total de enlaces":A[1],"Grado medio":A[2],"Coef. de Clust. red": A[6],"Dirigida?":A[9]})
+print(tabla)
+#%%
+for l in len(ess):
+    if 
+        
+   
